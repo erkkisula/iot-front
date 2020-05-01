fab131e37d0ddd570f304b8f0952238df943cd33
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-05-01 23:14:00 +0300

    Device generates values

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -15,6 +15,7 @@ interface DeviceProps {
 }
 
 export default class Device extends PureComponent<DeviceProps, DeviceState> {
+    timerID!: NodeJS.Timeout;
     constructor(props: any) {
         super(props);
         this.state = {
@@ -27,12 +28,69 @@ export default class Device extends PureComponent<DeviceProps, DeviceState> {
         };
     }
 
-    createRandomCurrentPower() {
+    createRandomCurrentPower = () => {
         let powerHigh = this.props.deviceInfo.devicePower * 1.5;
         return Math.floor(Math.random() * powerHigh) + this.state.powerLow;
-    }
+    };
 
-    componentDidMount = () => {
+    changeCurrentPower = () => {
+        const dir = Math.floor(Math.random() * 1000);
+        const change = Math.random() * 6;
+
+        let newPowerValue = this.state.currentPower;
+
+        if (dir <= 500) {
+            if (newPowerValue + change >= this.state.powerHigh) {
+                newPowerValue -= change;
+            } else {
+                if (newPowerValue < this.state.basePower) {
+                    let difference = this.state.basePower / newPowerValue;
+                    newPowerValue += change * difference;
+                } else {
+                    newPowerValue += change;
+                }
+            }
+        } else {
+            if (newPowerValue - change <= this.state.powerHigh) {
+                newPowerValue += change * 10;
+            } else {
+                if (newPowerValue < this.state.basePower) {
+                    let difference = newPowerValue / this.state.basePower;
+                    newPowerValue -= change * difference;
+                } else {
+                    newPowerValue -= change;
+                }
+            }
+        }
+
+        this.setState({
+            currentPower: Math.floor(newPowerValue),
+        });
+    };
+
+    changeCurrentNoise = () => {
+        const dir = Math.floor(Math.random() * 1000);
+        const change = Math.random() * 2 * (this.state.currentPower / this.state.basePower);
+        let newNoiseValue = this.state.currentPower * 0.08;
+        if (dir <= 500) {
+            newNoiseValue += change;
+        } else {
+            newNoiseValue -= change;
+        }
+
+        this.setState({
+            currentNoise: Math.round((newNoiseValue + Number.EPSILON) * 100) / 100,
+        });
+    };
+
+    update = () => {
+        if (this.state.dataLoaded) {
+            this.changeCurrentPower();
+            this.changeCurrentNoise();
+        }
+    };
+
+    setData = () => {
         this.setState({
             basePower: this.props.deviceInfo.devicePower,
             currentPower: this.createRandomCurrentPower(),
@@ -40,13 +98,26 @@ export default class Device extends PureComponent<DeviceProps, DeviceState> {
             dataLoaded: true,
         });
     };
-    render() {
-        setInterval(() => {
-            console.log("Working");
+
+    componentDidMount = () => {
+        this.setData();
+        this.timerID = setInterval(() => {
+            this.update();
         }, 1000);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    render() {
         return (
-            <div>
-                <div>I am device</div>
+            <div className="device-body">
+                <div className="device-info">
+                    <h2 className="device-name">{this.props.deviceInfo.deviceName}</h2>
+                    <div className="device-id">Seadme id: {this.props.deviceInfo.deviceId}</div>
+                    <div className="device-noise">Müra: {this.state.currentNoise}dB</div>
+                    <div className="device-power">Energia kasutus: {this.state.currentPower}W</div>
+                </div>
             </div>
         );
     }
